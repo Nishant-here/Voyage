90f2ac24d11c7ecdc53ae905302779a95f7dcd96
Nishant <nishantasha2@gmail.com>
2024-08-06 23:40:53 +0530

    Voyage

--- app/src/main/java/com/example/voyage/home_page.java
+++ app/src/main/java/com/example/voyage/home_page.java
@@ -1,24 +1,32 @@
 package com.example.voyage;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class home_page extends AppCompatActivity {
-
+//DrawerLayout menu_bar;
+//ImageButton menubtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+  /*      menu_bar=findViewById(R.id.menu_bar);
+        menubtn=findViewById(R.id.menubtn);
+        menubtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                menu_bar.open();
+            }
+        });*/
     }
 }
